a3d7f207ee3b89fc342c6e8bdfbe435db7953ea6
anonymous anonymous <yau.cheukting@gmail.com>
2022-03-14 16:45:34 +0000

    Validate presence of activities_logs.date

--- spec/models/activities_log_spec.rb
+++ spec/models/activities_log_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe ActivitiesLog, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:date) }
+
     it { should validate_presence_of(:user_id) }
 
     end
