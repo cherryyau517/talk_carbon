2fe5a01870e31fd9b5f9ac47af6c8b40c37d86d4
anonymous anonymous <yau.cheukting@gmail.com>
2022-03-14 16:45:34 +0000

    Validate inclusion of activity_rankings.month

--- app/models/activity_ranking.rb
+++ app/models/activity_ranking.rb
@@ -5,6 +5,8 @@ class ActivityRanking < ApplicationRecord
 
   # Validations
 
+  validates :month, :inclusion => { :in => [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" ]  }
+
   validates :user_id, :presence => true
 
   # Scopes
