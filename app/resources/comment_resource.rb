6b8f0c9c38074acc26e1d720a3f497134aae211b
anonymous anonymous <yau.cheukting@gmail.com>
2022-03-14 16:47:02 +0000

    Generate graphiti resources for comments

---
+++ app/resources/comment_resource.rb
@@ -0,0 +1,15 @@
+class CommentResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :log_id, :integer
+  attribute :commenter_id, :integer
+  attribute :comment, :string
+  attribute :date, :date
+  attribute :time, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
