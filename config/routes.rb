44326540ee932cb0f469da561c7104ceba98ae36
anonymous anonymous <yau.cheukting@gmail.com>
2022-03-14 16:45:58 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "activities_logs#index"
   resources :activity_types
   resources :activity_rankings
