031e86e4dfda8ceebcc466373ac245d18bf466c8
anonymous anonymous <yau.cheukting@gmail.com>
2022-03-14 16:46:06 +0000

    Whitelist columns in Following for ActiveAdmin

--- app/admin/followings.rb
+++ app/admin/followings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Following do
 
+ permit_params :follower_id, :followed_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
